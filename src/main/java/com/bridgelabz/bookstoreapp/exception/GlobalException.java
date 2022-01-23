bf9104fa3052fd37e497104eaece86372932afce
ABHIJIT <pooja.sd3@gmail.com>
2022-01-23 15:11:10 +0530

    [Refactored] Global Exception

--- src/main/java/com/bridgelabz/bookstoreapp/exception/GlobalException.java
+++ src/main/java/com/bridgelabz/bookstoreapp/exception/GlobalException.java
@@ -1,16 +1,33 @@
 package com.bridgelabz.bookstoreapp.exception;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.bridgelabz.bookstoreapp.dto.ResponseDTO;
 
 @ControllerAdvice
 public class GlobalException {
 	@ExceptionHandler(value = UserRegistrationException.class)
 	public ResponseEntity<Object> exception(UserRegistrationException exception) {
-		return new ResponseEntity<>("something went wrong", HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>("Exception while processing REST Request", HttpStatus.BAD_REQUEST);
    }
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> exception(MethodArgumentNotValidException exception) {
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMsg = errorList.stream()
+                .map(objErr -> objErr.getDefaultMessage())
+                .collect(Collectors.toList());
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", errMsg);
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
 	
 	@ExceptionHandler(value = NullPointerException.class)
 	public String exception(NullPointerException exception) {
